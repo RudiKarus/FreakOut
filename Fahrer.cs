ad2029369f34f690bf1f3e1bad59b2a8b6f3aad5
R_U_D_I <88041763+R-U-D-I@users.noreply.github.com>
2021-10-28 11:15:35 +0200

    Add files via upload

---
+++ Fahrer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fahrer : MonoBehaviour
+{
+    bool rundenZaehlenErlaubt = true;
+    bool inBereich = false;
+    int runde = 0;
+
+    float[] rundenzeitStart = new float[3];
+    public Text[] rundenzeitAnzeige = new Text[3];
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (rundenZaehlenErlaubt)
+        {
+
+            if (transform.position.x > 20 && transform.position.x < 30)
+            {
+                if (transform.position.z > 12 && transform.position.z < 12.5f)
+                {
+                    inBereich = true;
+                }
+                else if (inBereich && transform.position.z > 12.5f && transform.position.z < 13)
+                {
+                    runde++;
+                    if(runde>0 && runde <4)
+                    {
+                        rundenzeitStart[runde - 1] = Time.time;
+                    }
+                    Debug.Log(runde);
+                    inBereich = false;
+                    rundenZaehlenErlaubt = false;
+                    Invoke("RundenZaehlenErlauben", 20);
+                }
+            }
+        }
+        if(runde > 0 && runde < 4)
+        {
+            rundenzeitAnzeige[runde - 1].text = string.Format("Round {0,2:0}: {1,6:0.0} sec.", runde, Time.time - rundenzeitStart[runde - 1]);
+        }
+    }
+
+    void RundenZaehlenErlauben()
+    {
+        rundenZaehlenErlaubt = true;
+    }
+}
---
+++ Fahrzeug.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fahrzeug : MonoBehaviour
+{
+    float drehmomentFaktor = 30000;
+    public WheelCollider collVL, collVR, collHL, collHR;
+
+    float lenkwinkelFaktor = 40;
+    public GameObject radVL, radVR;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float drehmoment = drehmomentFaktor * Time.deltaTime * Input.GetAxis("Vertical");
+        collVL.motorTorque = drehmoment;
+        collVR.motorTorque = drehmoment;
+        collHL.motorTorque = drehmoment;
+        collHR.motorTorque = drehmoment;
+
+        float lenkwinkel = lenkwinkelFaktor * Input.GetAxis("Horizontal");
+        collVL.steerAngle = lenkwinkel;
+        collVR.steerAngle = lenkwinkel;
+        radVL.transform.localEulerAngles = new Vector3(radVL.transform.localEulerAngles.x, collVL.steerAngle, radVL.transform.localEulerAngles.z);
+        radVR.transform.localEulerAngles = new Vector3(radVR.transform.localEulerAngles.x, collVR.steerAngle, radVR.transform.localEulerAngles.z);
+    }
+}
