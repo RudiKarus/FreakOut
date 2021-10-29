ed739879034acb99d03dc4e3442bac166e148282
R_U_D_I <88041763+R-U-D-I@users.noreply.github.com>
2021-10-29 11:36:06 +0200

    Add files via upload

---
+++ Gefahren.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gefahren : MonoBehaviour
+{
+    float startX;
+    float aenderungX;
+
+    void Start()
+    {
+        startX = transform.position.x;
+        aenderungX = Random.Range(1.0f, 2.0f) * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        float newX = transform.position.x + aenderungX;
+        transform.position = new Vector3(newX, transform.position.y, 0);
+
+        if(newX > startX + 2 || newX < startX - 2)
+        {
+            aenderungX = aenderungX * -1;
+        }
+    }
+}
